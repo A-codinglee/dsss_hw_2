90bbc111784122272f6d86127bf9700b68d923ba
A-codinglee <156132163+A-codinglee@users.noreply.github.com>
2024-11-07 22:57:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    author="Sumin Lee",
+    author_email="sumin.s.lee@fau.de",
+    description="DSSS Assignment2 math_quiz",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com//A-codinglee/dsss_hw_2",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
 
